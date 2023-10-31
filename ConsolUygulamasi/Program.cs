15dc614bfb15c86222f35ed2280141a5e6f5a652
Lenovo <Lenovo@ABDULMUTTALIP>
2023-10-31 10:56:41 +0300

    Proje dosyası ekle.

---
+++ ConsolUygulamasi/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolUygulamasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba Dünya");
+            Console.Write("Abdulmuttilip ");
+            Console.WriteLine("Karaokur");
+        }
+    }
+}
